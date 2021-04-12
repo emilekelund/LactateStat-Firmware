17765a83f6d8b3efc377ea17b1b0d8b5b423b400
Emil Ekelund <emil.ekelund@outlook.com>
2021-04-12 14:46:22 +0200

    change ble advertise interval

--- main.c
+++ main.c
@@ -84,7 +84,7 @@
 #define MANUFACTURER_NAME "Chemical Sensing Group KTH" /**< Manufacturer. Will be passed to Device Information Service. */
 #define APP_ADV_INTERVAL 300                           /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
 
-#define APP_ADV_DURATION 36000  /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
+#define APP_ADV_DURATION 18000  /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
 #define APP_BLE_OBSERVER_PRIO 3 /**< Application's BLE observer priority. You shouldn't need to modify this value. */
 #define APP_BLE_CONN_CFG_TAG 1  /**< A tag identifying the SoftDevice BLE configuration. */
 
